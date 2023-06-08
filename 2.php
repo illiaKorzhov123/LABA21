d735da4f5ec37869f75926d476cb705f542afee0
illiaKorzhov123 <120598346+illiaKorzhov123@users.noreply.github.com>
2023-06-08 20:16:12 +0300

    Add files via upload

---
+++ 2.php
@@ -0,0 +1,32 @@
+<?php
+    // Обработка формы
+    if (isset($_POST['submit'])) {
+
+
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "nurses";
+        $selected_department_id = $_POST['department_id'];
+
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        
+
+        $stmt = $conn->prepare("SELECT n.name FROM nurse n
+                INNER JOIN nurse_ward nw ON nw.fid_nurse = n.id_nurse
+                WHERE nw.fid_ward = :selected");
+
+        $stmt->bindParam(':selected', $selected_department_id);       
+
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        
+        foreach ($result as $row) {
+            echo '<p>' . $row["name"] . '</p>';
+        }
+        
+
+        $conn = null;
+    }
+    ?>
---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Выбор медсестры</title>
+</head>
+<body>
+    <h1>Выберите медсестру:</h1>
+    <form method="post" action="1.php">
+        <select name="nurse_id">
+            <?php
+            // Подключение к базе данных
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "nurses";
+
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+           
+
+            $sql = "SELECT id_nurse, name FROM nurse";
+            $result = $conn->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+
+            
+            foreach ($result as $row) {
+                echo '<option value="' . $row["id_nurse"] . '">' . $row["name"] . '</option>';
+            }
+            
+
+            $conn = null;
+            ?>
+        </select>
+        <input type="submit" name="submit" value="Отправить">
+    </form>
+
+
+
+    <h1>Выберите отделение:</h1>
+    <form method="post" action="2.php">
+        <select name="department_id">
+            <?php
+
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "nurses";
+
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+            
+
+            $sql = "SELECT id_ward, name FROM ward";
+            $result = $conn->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+
+            
+                foreach ($result as $row) {
+                    echo '<option value="' . $row["id_ward"] . '">' . $row["name"] . '</option>';
+                }
+            
+
+            $conn = null;
+
+            ?>
+        </select>
+        <input type="submit" name="submit" value="Отправить">
+    </form>
+    
+
+
+    <h1>Выберите смену:</h1>
+    <form method="post" action="3.php">
+    <select name="shift">
+            <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "nurses";
+
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+            $sql = "SELECT DISTINCT `shift` FROM nurse";
+            $result = $conn->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+
+            
+            foreach ($result as $row) {
+                    echo '<option value="' . $row["shift"] . '">' . $row["shift"] . '</option>';
+            }
+            
+
+            $conn = null;
+
+            ?>
+        </select>
+        <input type="submit" name="submit" value="Отправить">
+    </form>
+
+</body>
+</html>
